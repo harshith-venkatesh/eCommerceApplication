c9df387b56455b24254d1f591d1b0b454901ebfd
Harshith7016 <harshithece7016@gmail.com>
2020-10-13 02:23:05 +0530

    axios data fetch initiation

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => {
-    return p._id === match.params.id;
-  });
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [match.params.id]);
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
@@ -16,10 +21,10 @@ const ProductScreen = ({ match }) => {
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid />
         </Col>
-        <Col md={3}>
+        <Col md={3} style={{ wordWrap: "break-word", padding: "5px 10px" }}>
           <ListGroup variant="flush">
             <ListGroup.Item>
-              <h3>{product.name}</h3>
+              <h3>{product.name} </h3>
             </ListGroup.Item>
             <ListGroup.Item>
               <Rating
@@ -28,7 +33,7 @@ const ProductScreen = ({ match }) => {
               />
             </ListGroup.Item>
             <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description} </ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={3}>
