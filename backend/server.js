401c8d88c857d1ecd3402919105a4769a1fb9282
Harshith7016 <harshithece7016@gmail.com>
2020-10-13 07:31:36 +0530

    db config and seeder file added

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,30 @@
-const express = require("express");
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import products from './data/products.js';
+import connectDB from './config/db.js';
+
 const app = express();
-const products = require("./data/products");
-app.get("/", (req, res) => {
-  res.send("API is running");
+dotenv.config();
+
+connectDB();
+app.get('/', (req, res) => {
+  res.send('API is running');
 });
 
-app.get("/api/products", (req, res) => {
+app.get('/api/products', (req, res) => {
   res.json(products);
 });
 
-app.get("/api/products/:id", (req, res) => {
+app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id == req.params.id);
   res.json(product);
 });
-app.listen(5000, console.log("Server running on port 5000"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+);
