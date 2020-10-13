d2b64cda0769e915a6cd714e9a140d51b3d636b0
Harshith7016 <harshithece7016@gmail.com>
2020-10-13 07:30:17 +0530

    changes on CSS

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,30 +1,28 @@
-import React from "react";
-import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import Rating from "./Rating";
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
   console.log(product._id);
   return (
-    <Card className="my-3 p-3 rounded">
+    <Card className='my-3 p-2 rounded'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" />
+        <Card.Img src={product.image} variant='top' />
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
-            <strong>{product.name}</strong>
+          <Card.Title>
+            <div style={{ textDecoration: 'empty' }}>{product.name}</div>
           </Card.Title>
         </Link>
-        <Card.Text as="div">
-          <div className="my-3">
-            <Rating
-              value={product.rating}
-              text={`${product.numReviews} reviews`}
-            />
-          </div>
+        <Card.Text as='div'>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
-        {product.price > 0 && <Card.Text as="h3">${product.price}</Card.Text>}
+        {product.price > 0 && <Card.Text as='h6'>${product.price}</Card.Text>}
       </Card.Body>
     </Card>
   );
