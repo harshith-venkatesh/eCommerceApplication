b06834bf391308c19646d627b637344116a05670
Harshith7016 <harshithece7016@gmail.com>
2020-10-14 14:49:02 +0530

    controller changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,3 +8,15 @@ const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
 });
+
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product Not Found');
+  }
+});
+
+export { getProductById, getProductById };
