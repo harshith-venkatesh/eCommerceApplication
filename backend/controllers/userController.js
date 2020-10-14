c8f506660ed85759779b8d69c65fa901d2ea04b1
Harshith7016 <harshithece7016@gmail.com>
2020-10-14 15:00:56 +0530

    user Controller added for authentication

---
+++ backend/controllers/userController.js
@@ -0,0 +1,12 @@
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+//@desc Auth user and get token
+//@route POST /api/users/login
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  res.send({ email, password });
+});
+
+export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoute from './routes/productRoutes.js';
+import userRoutes from './routes/userRoute.js';
 import { notFound, errorHandler } from './middleware/errorMiddleWare.js';
+
 const app = express();
+
+app.use(express.json());
 dotenv.config();
 connectDB();
 
@@ -12,10 +16,10 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 app.use('/api/products', productRoute);
+app.use('/api/users', userRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.use(notFound);
-
 app.use(errorHandler);
 
 app.listen(
