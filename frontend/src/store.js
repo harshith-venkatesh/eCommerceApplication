5bfb4c5d071c99ea8d9c5e6f9aa9008aa8a6b7da
Harshith7016 <harshithece7016@gmail.com>
2020-10-22 07:04:46 +0530

    profile screeen created

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,13 +6,18 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducer';
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
+import {
+  userDetailsReducer,
+  userLoginReducer,
+  userRegisterReducer,
+} from './reducers/userReducers';
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
