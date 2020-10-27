9764c5213b6b42b64b4f0e6b201d5fc0e368a5ee
Harshith7016 <harshithece7016@gmail.com>
2020-10-27 14:42:03 +0530

    minor bug fix

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,7 @@
 import express from 'express';
 import { addOrderItems } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleWare.js';
-
 const router = express.Router();
-
-router.post('/').post(protect, addOrderItems);
+router.post('/',protect, addOrderItems);
 
 export default router;
