7875f00c615e00e4d90b19415ecae720b3d590bd
Harshith7016 <harshithece7016@gmail.com>
2020-10-29 18:17:18 +0530

    ui changes to display my orders

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,6 +13,7 @@ import {
   userUpdateProfileReducer,
 } from './reducers/userReducers';
 import {
+  getMyOrderReducer,
   orderCreateReducer,
   orderDetailsReducer,
   orderPayReducer,
@@ -29,6 +30,7 @@ const reducer = combineReducers({
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
+  orderMyList: getMyOrderReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
