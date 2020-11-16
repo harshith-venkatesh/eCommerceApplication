afff3f603b5843363092da96b33567da1b4305e5
Harshith7016 <harshithece7016@gmail.com>
2020-11-16 08:57:54 +0530

    isadmin check backend added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -97,4 +97,12 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+//@desc Get all users
+//@route GET /api/users
+// @access private/admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.send(users);
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,15 +2,16 @@ import express from 'express';
 import {
   authUser,
   getUserProfile,
+  getUsers,
   registerUser,
   updateUserProfile,
 } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleWare.js';
+import { admin, protect } from '../middleware/authMiddleWare.js';
 
 const router = express.Router();
-
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
-router.post('/', registerUser);
+
 router
   .route('/profile')
   .get(protect, getUserProfile)
