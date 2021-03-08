39071699f975fee68b58cec2288a0c5fbcfb5c1c
Harshith7016 <harshithece7016@gmail.com>
2021-03-08 12:16:38 +0530

    feat: delete product added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import {
+  deleteProduct,
   getProductById,
   getProducts,
 } from '../controllers/productController.js';
+import { admin, protect } from '../middleware/authMiddleWare.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
