fa49a2f783bc641831b4f8c65bca4d9ca002b588
Harshith7016 <harshithece7016@gmail.com>
2021-03-24 14:34:13 +0530

    minor update

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -28,14 +28,16 @@ const HomeScreen = ({match}) => {
   return (
     <>
     <Meta />
-    {!keyword ? <ProductCarousal /> : <Link to='/' className='btn btn-light'>Go Back</Link> }
-      <h1>Latest Products</h1>
+    
+    
       {loading ? (
         <Loader />
       ) : error ? (
         <Message variant='danger'>{error}</Message>
       ) : (
         <>
+        {!keyword ? <ProductCarousal /> : <Link to='/' className='btn btn-light'>Go Back</Link> }
+        <h1>Latest Products</h1>
         <Row>
           {products.map((product) => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
