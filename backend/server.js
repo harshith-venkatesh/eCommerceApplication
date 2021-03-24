227dcc7b4c8fc4a54202805cf2be949f8d64ffcb
Harshith7016 <harshithece7016@gmail.com>
2021-03-24 12:52:32 +0530

    production deployment fix

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleWare.js';
+
 import productRoute from './routes/productRoutes.js';
 import userRoutes from './routes/userRoute.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleWare.js';
+
 
 dotenv.config();
 connectDB();
@@ -29,6 +31,10 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+
+const __dirname = path.resolve();
+app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
+
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname,'/frontend/build')));
   app.get('*', (req,res) => {
@@ -36,8 +42,7 @@ if(process.env.NODE_ENV === 'production'){
   })
 }
 
-const __dirname = path.resolve();
-app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+
 const PORT = process.env.PORT || 5000;
 
 app.use(notFound);
