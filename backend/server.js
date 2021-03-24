290f07b45bb167cf8094d9c3dee66b4052e2401a
Harshith7016 <harshithece7016@gmail.com>
2021-03-24 14:21:09 +0530

    prod fix

--- backend/server.js
+++ backend/server.js
@@ -38,10 +38,12 @@ if(process.env.NODE_ENV === 'production'){
   app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
   })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
 }
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
+
 
 const PORT = process.env.PORT || 5000;
 
